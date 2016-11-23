9e4315025d82b9c481d66acb9df3d8814122aec7
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-23 11:51:17 +0000

    Created form views

--- lib/app.rb
+++ lib/app.rb
@@ -7,4 +7,15 @@ class Battle < Sinatra::Base
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  get '/start-battle' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
+  end
+
 end
