c9f5a97484fc72ce3a316a99023c8e60f18e1560
Chris Cooper <cjcooper252@gmail.com>
2016-11-24 14:42:11 +0000

    skinny

--- app.rb
+++ app.rb
@@ -10,23 +10,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-      @player_1_name = $player_1.name
-      @player_2_name = $player_2.name
-      @player_1_points = $player_1.hit_points
-      @player_2_points = $player_2.hit_points
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    game = Game.new
-    @player_2_name = $player_2.name
-    game.attack($player_2)
+    @game = $game
+    @game.attack(@game.player2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
 require './game'
 
 RSpec.describe Game do
-  subject(:game) { described_class.new }
+
+  subject(:game) { described_class.new(player1, player2) }
   let (:player1) { double :player }
+  let (:player2) { double :player }
+
   describe '#attack' do
     it 'attacks the other player' do
       expect(player1).to receive(:deduct)
       game.attack(player1)
     end
   end
+
+  it 'should be initialized with 2 players' do
+    expect(game.player1).to eq player1
+  end
+
 end
