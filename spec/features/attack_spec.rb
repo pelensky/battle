49650a1ed9f98a3ab936bf72fc608ea45aefa678
Chris Cooper <cjcooper252@gmail.com>
2016-11-24 11:21:21 +0000

    adds attack confirmation

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.feature "attacks a player" do
   scenario "attacks player 2" do
     sign_in_and_play
-    click_button 'Attack'
+    click_button("Attack")
     expect(page).to have_content("You have attacked Elizabeth")
   end
 end
